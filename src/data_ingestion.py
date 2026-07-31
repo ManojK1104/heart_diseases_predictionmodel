ea071596034b3ff7bb0f4e907e742988a7af5697
ManojK1104 <manojrkushwaha85@gmail.com>
2026-07-31 14:53:58 +0530

    done

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -2,5 +2,5 @@ import numpy as np
 import pandas as pd
 
 def ingestion():
-    df=pd.read_csv(r"E:\heart_diseases_predictionmodel\data\heart_disease_risk_2026.csv")
+    df=pd.read_csv(r"https://github.com/ManojK1104/heart_diseases_predictionmodel/blob/main/data/heart_disease_risk_2026.csv")
     return df
